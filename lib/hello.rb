4d17db495e8e04ebeb1e96d038df4eb07be0f3d9
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 20:48:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,8 @@
 def hello_t(array)
   if block_given?  
-    yield array[i]
-    i += 1 
-  end 
-  array 
+    i = 0 
+    while i < array.length 
+      yield(array[i])
 end
 
 # call your method here!
